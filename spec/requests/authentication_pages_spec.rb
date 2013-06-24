9c82425714c8aa9afc634fd3cc43c9b31b0a1f87
Scryb3 <scrybe@gmail.com>
2013-06-23 20:43:46 -0500

    create remember_token on users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,6 +11,7 @@ describe "Authentication" do
     it { should have_title('Sign in') }
   end
   describe "signin" do
+
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -18,6 +19,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
   describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
